5661b885530078e83af8bb6f81d6781939859b1c
unknown <rajan.kaneria@gmail.com>
2017-08-01 11:15:05 +0530

    CI file structure changes

---
+++ application/views/includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msapplication-tap-highlight" content="no">
+    <title>CodeIgniter - <?php echo $pageTitle; ?></title>
+    <!-- CSS-->
+    <link href="<?php echo base_url(); ?>html/css/style.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo base_url(); ?>html/js/script.js"></script>
+    <?php foreach($stylesheet as $fileName){ ?>
+    <link href="<?php echo base_url(); ?>html/css/<?php echo $fileName; ?>" rel="stylesheet">
+    <?php } ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
